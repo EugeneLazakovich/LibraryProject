c6d4ad08183c8f08501f8dd61d50781a2b296646
EugeneLazakovich EugeneLazakovich <e.lazakovich@center-ecm.com>
2022-06-30 21:30:26 +0300

    Add project files.

---
+++ Lesson1/Book.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Lesson1
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+
+        public int YearOfIssue { get; set; }
+    }
+}
---
+++ Lesson1/Controllers/BookController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lesson1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController : ControllerBase
+    {
+        private static List<Book> Books { get; set; }
+
+        static BookController()
+        {
+            Books = new List<Book>();
+        }
+
+        private readonly ILogger<BookController> _logger;
+
+        public BookController(ILogger<BookController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return Books;
+        }
+
+        [HttpGet("{id}")]
+        public Book GetById(Guid id)
+        {
+            return Books.Where(c => c.Id == id).FirstOrDefault();
+        }
+
+        [HttpPost]
+        public void Create(Book book)
+        {
+            book.Id = Guid.NewGuid();
+            Books.Add(book);
+        }
+
+        [HttpPut("{id}")]
+        public bool Update(Guid id, Book book)
+        {
+            Book bookFromList = GetById(id);
+            book.Id = id;
+            if (bookFromList != null)
+            {
+                var index = Books.IndexOf(bookFromList);
+                Books[index] = book;
+            }
+            return bookFromList != null;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(Guid id)
+        {
+            var book = GetById(id);
+            Books.Remove(book);
+        }
+    }
+}
